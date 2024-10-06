ce9af7a669015a2a326c1f4cfed3acdc30a7a3a8
supriyopurkait <supriyopurkait7@gmail.com>
2024-10-06 20:37:06 +0530

    chore: intution process

--- array_operation/array_element_present_n_2.java
+++ array_operation/array_element_present_n_2.java
@@ -41,3 +41,10 @@ class solution {
     // Map<int,int> optimal(int arr[], int length){
     // }
 }
+
+/*The intuition behind this algorithm names moorse voting algorithm is 
+ * First step is we will consider the element in index 0 then we will take another variable which will count the number of times the element will present
+ *  Second rule is if the next value is is equal to the value which we have considered then we increase the value of the count variable
+ * 3rd rule is if the variable or the element in array index means we have considered the element in zero index if we find some other elements in one index then we will decrease the value of the count
+ * 4th rule is when the count value will be is equal to zero then we will interchange or initialize next new value at that present index means if we are present in 5th index or 4th index the value will be initialized the value of that particular index
+ */
